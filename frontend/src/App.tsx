03f3413c18b6dffe14d33b0a32193a49a617f5e4
Solène <81546747+solenedel@users.noreply.github.com>
2025-06-24 09:29:52 +0200

    add basic loading state and status

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,35 +4,48 @@ import './App.css';
 
 // types ---------------------------------------------------------------------
 
-type Dog = {
+interface Dog {
   message: string;
-};
+}
+
+interface Status {
+  singleDogStatus: 'idle' | 'loading' | 'success' | 'error';
+  multipleDogsStatus: 'idle' | 'loading' | 'success' | 'error';
+}
 
 // console.log('🌍🌍🌍🌍 dogImage', dogImage);
 
 function App() {
   const [singleDogUrl, setSingleDogUrl] = useState<string | null>(null);
   const [singleDogBreed, setSingleDogBreed] = useState<string | null>(null);
+  const [status, setStatus] = useState<Status>({
+    singleDogStatus: 'idle',
+    multipleDogsStatus: 'idle',
+  });
 
   // handleGetSingleDog ---------------------------------------------------------
 
+  // to do - turn into custom hook
   const handleGetSingleDog = async (): Promise<void> => {
+    setStatus({ ...status, singleDogStatus: 'loading' });
     try {
       // fetch from backend
       const response = await axios.get(`/api/random-dog`);
       const dogImage: Dog = response.data;
 
       setSingleDogUrl(dogImage.message);
+      setStatus({ ...status, singleDogStatus: 'success' });
     } catch (error) {
       if (axios.isAxiosError(error)) {
         console.error('Axios error fetching single dog:', error.response?.data);
       } else {
         console.error('Unexpected error fetching single dog:', error);
       }
+      setStatus({ ...status, singleDogStatus: 'error' });
     }
   };
 
-  // useEffect: extract dog breed form url --------------------------------------
+  // useEffect: extract dog breed form url -------------------------------------------------------------
 
   useEffect(() => {
     // extract breed from url
@@ -42,14 +55,18 @@ function App() {
 
       if (breedName) {
         const formattedBreedName = breedName.replace('-', ' ');
+
         setSingleDogBreed(formattedBreedName);
       }
     };
-
     if (singleDogUrl) {
       handleGetDogBreed();
     }
   }, [singleDogUrl]);
+  // note: useEffect cleanup is unnecessary here because it is a synchronous operation
+  // note: useCallback is also unnecessary because the function is not passed as a dependency and is simple
+
+  // RENDER -------------------------------------------------------------------------------
 
   return (
     <>
@@ -58,6 +75,7 @@ function App() {
         <h2>Get one random dog</h2>
         <button onClick={handleGetSingleDog}>Get dog</button>
       </section>
+      {status.singleDogStatus === 'loading' && <p>Loading...</p>}
       {singleDogUrl ? (
         <>
           <img src={singleDogUrl} alt="Random dog" width={400} />
