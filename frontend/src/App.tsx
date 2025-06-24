604bf217d77d5c164de0e8fce4e9eb162c4d3f18
Solène <81546747+solenedel@users.noreply.github.com>
2025-06-24 11:42:01 +0200

    working on get multiple dogs in frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,9 +8,11 @@ interface Dog {
   message: string;
 }
 
+type statusOptions = 'idle' | 'loading' | 'success' | 'error';
+
 interface Status {
-  singleDogStatus: 'idle' | 'loading' | 'success' | 'error';
-  multipleDogsStatus: 'idle' | 'loading' | 'success' | 'error';
+  singleDogStatus: statusOptions;
+  multipleDogsStatus: statusOptions;
 }
 
 // console.log('🌍🌍🌍🌍 dogImage', dogImage);
@@ -18,6 +20,7 @@ interface Status {
 function App() {
   const [singleDogUrl, setSingleDogUrl] = useState<string | null>(null);
   const [singleDogBreed, setSingleDogBreed] = useState<string | null>(null);
+  const [multipleDogs, setMultipleDogs] = useState<Dog[]>([]);
   const [status, setStatus] = useState<Status>({
     singleDogStatus: 'idle',
     multipleDogsStatus: 'idle',
@@ -45,6 +48,29 @@ function App() {
     }
   };
 
+  // handleGetMultipleDogs ---------------------------------------------------------
+
+  const handleGetMultipleDogs = async (count: number): Promise<void> => {
+    setStatus({ ...status, multipleDogsStatus: 'loading' });
+
+    try {
+      const response = await axios.get(`/api/multiple-dogs?count=${count}`);
+      const multipleDogs: Dog[] = response.data.message;
+
+      console.log('🌍🌍🌍🌍 multipleDogs', multipleDogs);
+
+      setMultipleDogs(multipleDogs);
+      setStatus({ ...status, multipleDogsStatus: 'success' });
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.error('Axios error fetching single dog:', error.response?.data);
+      } else {
+        console.error('Unexpected error fetching single dog:', error);
+      }
+      setStatus({ ...status, multipleDogsStatus: 'error' });
+    }
+  };
+
   // useEffect: extract dog breed form url -------------------------------------------------------------
 
   useEffect(() => {
@@ -66,28 +92,57 @@ function App() {
   // note: useEffect cleanup is unnecessary here because it is a synchronous operation
   // note: useCallback is also unnecessary because the function is not passed as a dependency and is simple
 
-  // RENDER -------------------------------------------------------------------------------
+  //useEffect: keep track of status -------------------------------------------------------------
+
+  useEffect(() => {
+    console.log('🌍🌍🌍🌍 status', status);
+  }, [status]);
+
+  // RENDER --------------------------------------------------------------------------------------
 
   return (
     <>
       {/* GET SINGLE DOG SECTION */}
       <section>
         <h2>Get one random dog</h2>
-        <button onClick={handleGetSingleDog}>Get dog</button>
+        <button
+          disabled={status.singleDogStatus === 'loading'}
+          onClick={handleGetSingleDog}>
+          Get dog
+        </button>
       </section>
-      {status.singleDogStatus === 'loading' && <p>Loading...</p>}
+      {status.singleDogStatus === 'loading' && (
+        <p
+          style={{
+            width: '400px',
+            height: '400px',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          Loading...
+        </p>
+      )}
       {singleDogUrl ? (
         <>
-          <img src={singleDogUrl} alt="Random dog" width={400} />
+          <img src={singleDogUrl} loading="lazy" alt="Random dog" width={400} />
           {singleDogBreed && <p>{singleDogBreed}</p>}
         </>
       ) : (
         <p>No dog selected yet.</p>
       )}
-      {/* <section>
+      {/* MULTIPLE DOGS SECTION */}
+      <section>
         <h2>Get multiple dogs</h2>
-
-      </section> */}
+        <div>
+          <input type="number" min={1} max={10} />
+          <button
+            disabled={status.multipleDogsStatus === 'loading'}
+            onClick={() => handleGetMultipleDogs(3)}>
+            Get dogs
+          </button>
+        </div>
+      </section>
     </>
   );
 }
